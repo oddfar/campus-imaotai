56419934aee978cc65a3d99bbda4b298c9b42227
shangdezhong <shangdz@chinatelecom.cn>
2024-01-15 17:28:00 +0800

    设置+8时区，避免因为时区问题导致预约时间不正确

--- campus-modular/src/main/java/com/oddfar/campus/CampusApplication.java
+++ campus-modular/src/main/java/com/oddfar/campus/CampusApplication.java
@@ -3,6 +3,9 @@ package com.oddfar.campus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 /**
  * GitHub: https://github.com/oddfar/campus-imaotai
  * @author oddfar
@@ -11,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CampusApplication {
 
     public static void main(String[] args) {
+        //设置+8时区，避免因为时区问题导致预约时间不正确
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.of("+8")));
         SpringApplication.run(CampusApplication.class, args);
     }
 
