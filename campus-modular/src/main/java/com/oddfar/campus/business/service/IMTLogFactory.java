0efb1eed4f7bc7429f74914965afef6d26a791fc
LeiDuang <47079340+LeiDuang@users.noreply.github.com>
2024-03-25 18:25:56 +0800

    Update IMTLogFactory.java

--- campus-modular/src/main/java/com/oddfar/campus/business/service/IMTLogFactory.java
+++ campus-modular/src/main/java/com/oddfar/campus/business/service/IMTLogFactory.java
@@ -35,7 +35,11 @@ public class IMTLogFactory {
         //推送
         PushPlusApi.sendNotice(iUser, operLog);
     }
-
+    
+    public static void reservation(IUser iUser, String logContent , String msghead) {
+        reservation(iUser, logContent , msghead + "成功"，msghead + "失败");
+    }
+    
     public static void reservation(IUser iUser, String logContent , String SCUmsghead , String FAUmsghead) {
         //{"code":2000,"data":{"successDesc":"申购完成，请于7月6日18:00查看预约申购结果","reservationList":[{"reservationId":17053404357,"sessionId":678,"shopId":"233331084001","reservationTime":1688608601720,"itemId":"10214","count":1}],"reservationDetail":{"desc":"申购成功后将以短信形式通知您，请您在申购成功次日18:00前确认支付方式，并在7天内完成提货。","lotteryTime":1688637600000,"cacheValidTime":1688637600000}}}
         ILog operLog = new ILog();
